5ae0c7343b417b25d592cd049f51ce400e5f1a2d
TomasT04 <tomas.trogliero04@gmail.com>
2025-09-23 17:48:02 -0300

    TP3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageUploader from "./componentes/CargadorImagen";
+import emailjs from "emailjs-com";
 
-function App() {
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function Home() {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Cargador de Imagen en React</h1>
+      <h1>Home</h1>
+      <h2>Cargador de Imagen </h2>
       <ImageUploader />
     </div>
   );
 }
-export default App;
+
+function Contacto() {
+  const [name, setNombre] = useState("");
+  const [email, setCorreo] = useState("");
+  const [message, setMensaje] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        "service_fhv91a6",   // 👈 ID de tu servicio en EmailJS
+        "template_x0h698h",  // 👈 ID de tu template en EmailJS
+        { name, email, message },
+        "2ClqmuT9yTWubYfNJ"       // 👈 tu user ID de EmailJS
+      )
+      .then(() => {
+        alert("✅ Mensaje enviado con éxito");
+        setNombre("");
+        setCorreo("");
+        setMensaje("");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("❌ Error al enviar el mensaje");
+      });
+  };
+
+  return (
+    <div style={{ maxWidth: "400px", margin: "0 auto" }}>
+      <h1>📞 Contacto</h1>
+      <form onSubmit={handleSubmit}>
+        <br />
+        <label>Nombre </label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setNombre(e.target.value)}
+          required
+        />
+        <br />
+        <label>Correo</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setCorreo(e.target.value)}
+          required
+        />
+        <br />
+        <label>Mensaje</label>
+        <textarea
+          value={message}
+          onChange={(e) => setMensaje(e.target.value)}
+          required
+        />
+
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      {/* Menú de navegación */}
+      <nav style={{ display: "flex", gap: "1rem" }}>
+        <Link to="/">Home</Link>
+        <Link to="/contacto">Contacto</Link>
+      </nav>
+
+      {/* Definición de rutas */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </Router>
+  );
+}
